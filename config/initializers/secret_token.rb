3b5b22cfc6a108bb569a26aa67d107ceb000f269
Angel Solorio <angel.solorio@crowdint.com>
2013-03-01 12:55:18 -0600

    First commit. The depot project before code the tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '7448cc99389998c35be43cf569b5e3cde67d176db5979e6a61fb7bf04f5ff5eefad83000dba02f110fc421ec5c0ea38baa97a46c255dd8dcb56f937d13111065'
